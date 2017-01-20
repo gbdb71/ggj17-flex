c6ff29441c028294426e58c8fce022935b73f2be
Plus Pingya <pluspingya@gmail.com>
2017-01-21 01:34:48 +1100

    Waving algorithm completed

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,33 +1,135 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class Board : MonoBehaviour {
 
-	public GameObject Cube;
+	public enum Neighbour {
+		North,
+		East,
+		South,
+		West
+	};
+
+	public const int Row = 20;
+	public const int Column = 20;
+
+	public float AditionDelay = 0.1f;
+
+
+	public Cube[,] Cubes;
+
+	public GameObject MasterCube;
 
 	// Use this for initialization
 	void Start () {
-		CreateBoard ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
 		
+		CreateBoard ();
+
 	}
 
 	void CreateBoard() {
-	
+		
+		Cubes = new Cube[Row, Column];
 
-		for (int i = 0, row = 10; i < row; i++) {
-			for (int j = 0, column = 10; j < column; j++) {
-				var cube = GameObject.Instantiate (Cube);
-				cube.transform.parent = Cube.transform.parent;
-				cube.transform.position = new Vector3 (j, 0, i);
+		for (int i = 0; i < Row; i++) {
+			for (int j = 0; j < Column; j++) {
+				
+				var clone = GameObject.Instantiate (MasterCube);
+				clone.transform.parent = MasterCube.transform.parent;
+				float beginX = -Row * 0.5f;
+				float beginZ = -Column * 0.5f;
+				clone.transform.position = new Vector3 (beginX + j * 1f, 0, beginZ + i * 1f);
+
+				var cube = clone.GetComponent<Cube> ();
+				cube.OnSelected = OnSelected;
+				cube.SetCoordinate (j, i);
+
+				Cubes [j, i] = cube;
+
 			}
 		}
 
-		Cube.SetActive (false);
+		MasterCube.SetActive (false);
+
+	}
+
+	void OnSelected(Cube cube) {
+
+		Push (cube);
+
+	}
+
+	void Push(Cube cube) {
+		var already = new List<Cube> ();
+		Push (new List<Cube> { cube }, already, 0.0f);
+	}
+
+	void Push(List<Cube> cubes, List<Cube> already, float delay) {
+
+		var neigbourCubes = new List<Cube> ();
+
+		foreach (var cube in cubes) {
+			if (already.Contains(cube))
+				continue;
+			cube.Wave (delay);
+			already.Add (cube);
+
+			var nbs = GetNeighbours (cube);
+			foreach (var nb in nbs) {
+				if (already.Contains(nb))
+					continue;
+				neigbourCubes.Add(nb);
+			}
+		}
+
+		if (neigbourCubes.Count == 0)
+			return;
+		
+		delay += AditionDelay;
+		Push (neigbourCubes, already, delay);
+
+	}
+
+	List<Cube> GetNeighbours(Cube cube) {
+	
+		var ret = new List<Cube> ();
+
+		foreach (Neighbour n in Enum.GetValues(typeof(Neighbour))) {
+			int neighbourRow, neighbourColumn;
+			switch (n) {
+			default:
+			case Neighbour.North:
+				neighbourRow = cube.Row + 1;
+				neighbourColumn = cube.Column;
+				break;
+			case Neighbour.East:
+				neighbourRow = cube.Row;
+				neighbourColumn = cube.Column + 1;
+				break;
+			case Neighbour.South:
+				neighbourRow = cube.Row - 1;
+				neighbourColumn = cube.Column;
+				break;
+			case Neighbour.West:
+				neighbourRow = cube.Row;
+				neighbourColumn = cube.Column-1;
+				break;
+			}
+			if (neighbourRow < 0 
+				|| neighbourRow >= Row 
+				|| neighbourColumn < 0 
+				|| neighbourColumn >= Column)
+				continue;
+			var neighbourCube = Cubes [neighbourColumn, neighbourRow];
+			if (neighbourCube == null)
+				continue;
+			ret.Add (neighbourCube);
+		}
+
+		return ret;
 
 	}
 
--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,16 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Cube : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float WaveMax = 1.0f;
+	public float WavePower = 5f;
+
+	public Action<Cube> OnSelected;
+
+	public int Column { get; private set; }
+	public int Row { get; private set; }
+
+	public void SetCoordinate(int column, int row) {
+		Column = column;
+		Row = row;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	public void Wave(float delay) {
 		
+		var position = gameObject.transform.position;
+
+		var index = 0;
+		var path = new Vector3[5];
+
+		var power = WavePower;
+		var down = true;
+
+		while (power > 0f && index < path.Length) {
+
+			if (down) {
+				if (position.y - power < -WaveMax) {
+					power -= -WaveMax - position.y;
+					position.y = -WaveMax;
+				} else {
+					position.y -= power;
+					power = 0;
+				}
+			} else { //up
+				if (position.y + power > WaveMax) {
+					power -= WaveMax - position.y;
+					position.y = WaveMax;
+				}else {
+					position.y += power;
+					power = 0f;
+				}
+			}
+
+			path [index] = position;
+			index++;
+			down = !down;
+		}
+
+		while (index < path.Length) {
+			path [index] = position;
+			index++;
+		}
+
+		iTween.MoveTo (gameObject, iTween.Hash("path", path, "easetype", iTween.EaseType.linear, "delay", delay, "time", WavePower * 0.5f));
+
 	}
+
+	void OnMouseOver() {
+		if(Input.GetMouseButtonDown(0)){
+			if (OnSelected != null)
+				OnSelected (this);
+		}
+	}
+
 }
