65f13ca74989349904a2a6de7121c054fd5dc540
Plus Pingya <pluspingya@gmail.com>
2017-01-20 21:27:20 +1100

    initial project

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+	public GameObject Cube;
+
+	// Use this for initialization
+	void Start () {
+		CreateBoard ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CreateBoard() {
+	
+
+		for (int i = 0, row = 10; i < row; i++) {
+			for (int j = 0, column = 10; j < column; j++) {
+				var cube = GameObject.Instantiate (Cube);
+				cube.transform.parent = Cube.transform.parent;
+				cube.transform.position = new Vector3 (j, 0, i);
+			}
+		}
+
+		Cube.SetActive (false);
+
+	}
+
+}
